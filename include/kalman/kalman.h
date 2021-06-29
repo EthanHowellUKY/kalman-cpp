808ca27911d243b347d22f3fd3863ac432f38206
Ethan Howell <ehowell@bastiansolutions.com>
2021-06-28 22:52:30 -0400

    Implemented in-house matrix models and linear algebra library.

--- include/kalman/kalman.h
+++ include/kalman/kalman.h
@@ -3,95 +3,83 @@
 #define KALMAN_H
 
 /**
-* Kalman filter implementation using Eigen. Based on the following
-* introductory paper:
-*
-*     http://www.cs.unc.edu/~welch/media/pdf/kalman_intro.pdf
-*
-* @author: Hayk Martirosyan
-* @date: 2014.11.15
-*/
+ * Kalman filter implementation using Eigen. Based on the following
+ * introductory paper:
+ *
+ *     http://www.cs.unc.edu/~welch/media/pdf/kalman_intro.pdf
+ *
+ * @author: Hayk Martirosyan
+ * @date: 2014.11.15
+ */
 
-#include <Eigen/Dense>
+#include "LinearAlgebra/Matrix.h"
+#include "LinearAlgebra/SolverFactory.h"
 
-#pragma once
-
-class KalmanFilter {
+class KalmanFilter
+{
 
 public:
+    // -------------------------------- //
+    //           CONSTRUCTORS           //
+    // -------------------------------- //
+    KalmanFilter(
+        double dt,
+        const Matrix &A,
+        const Matrix &C,
+        const Matrix &Q,
+        const Matrix &R,
+        const Matrix &P);
 
-  /**
-  * Create a Kalman filter with the specified matrices.
-  *   A - System dynamics matrix
-  *   C - Output matrix
-  *   Q - Process noise covariance
-  *   R - Measurement noise covariance
-  *   P - Estimate error covariance
-  */
-  KalmanFilter(
-      double dt,
-      const Eigen::MatrixXd& A,
-      const Eigen::MatrixXd& C,
-      const Eigen::MatrixXd& Q,
-      const Eigen::MatrixXd& R,
-      const Eigen::MatrixXd& P
-  );
+    KalmanFilter();
 
-  /**
-  * Create a blank estimator.
-  */
-  KalmanFilter();
+    // -------------------------------- //
+    //         PUBLIC FUNCTIONS         //
+    // -------------------------------- //
 
-  /**
-  * Initialize the filter with initial states as zero.
-  */
-  void init();
+    // ------------ GETTERS ----------- //
+    Matrix state();
+    double time();
 
-  /**
-  * Initialize the filter with a guess for initial states.
-  */
-  void init(double t0, const Eigen::VectorXd& x0);
-
-  /**
-  * Update the estimated state based on measured values. The
-  * time step is assumed to remain constant.
-  */
-  void update(const Eigen::VectorXd& y);
-
-  /**
-  * Update the estimated state based on measured values,
-  * using the given time step and dynamics matrix.
-  */
-  void update(const Eigen::VectorXd& y, double dt, const Eigen::MatrixXd A);
-
-  /**
-  * Return the current state and time.
-  */
-  Eigen::VectorXd state() { return x_hat; };
-  double time() { return t; };
+    // ------------ SETTERS ----------- //
+    void init();
+    void init(double t0, const Matrix &x0);
+    void update(const Matrix &y);
+    void update(const Matrix &y, double dt, const Matrix A);
 
 private:
+    // -------------------------------- //
+    //        PRIVATE FUNCTIONS         //
+    // -------------------------------- //
+    void setInitState(const double &t, const Matrix &P);
 
-  // Matrices for computation
-  Eigen::MatrixXd A, C, Q, R, P, K, P0;
+    // -------------------------------- //
+    //        PRIVATE VARIABLES         //
+    // -------------------------------- //
 
-  // System dimensions
-  int m, n;
+    // Matrices for computation
+    Matrix A, C, Q, R, P, K, P0;
+    Matrix A_t, b_t;
 
-  // Initial and current time
-  double t0, t;
+    // System dimensions
+    int m, n;
 
-  // Discrete time step
-  double dt;
+    // Initial and current time
+    double t0, t;
 
-  // Is the filter initialized?
-  bool initialized;
+    // Discrete time step
+    double dt;
 
-  // n-size identity
-  Eigen::MatrixXd I;
+    // Is the filter initialized?
+    bool initialized;
 
-  // Estimated states
-  Eigen::VectorXd x_hat, x_hat_new;
+    // n-size identity
+    Matrix I;
+
+    // Estimated states
+    Matrix x_hat, x_hat_new;
+
+    // Linear Systems solver
+    Solver *m_solver = SolverFactory::create(SOLVER::LUDECOMP);
 };
 
 #endif // KALMAN_H
--- src/kalman.cpp
+++ src/kalman.cpp
@@ -10,41 +10,84 @@
 
 #include "kalman/kalman.h"
 
+// -------------------------------- //
+//        PRIVATE FUNCTIONS         //
+// -------------------------------- //
+
+/**
+ * @brief Set the initial parameters of the estimator.
+ * 
+ */
+void KalmanFilter::setInitState(const double &t, const Matrix &P)
+{
+    this->P = P;
+    this->t0 = t;
+    this->t = t;
+    this->initialized = true;
+}
+
+// -------------------------------- //
+//           CONSTRUCTORS           //
+// -------------------------------- //
+
+/**
+ * @brief Create a Kalman filter with the specified matrices.
+ *   A - System dynamics matrix
+ *   C - Output matrix
+ *   Q - Process noise covariance
+ *   R - Measurement noise covariance
+ *   P - Estimate error covariance
+ */
 KalmanFilter::KalmanFilter(
     double dt,
-    const Eigen::MatrixXd &A,
-    const Eigen::MatrixXd &C,
-    const Eigen::MatrixXd &Q,
-    const Eigen::MatrixXd &R,
-    const Eigen::MatrixXd &P)
+    const Matrix &A,
+    const Matrix &C,
+    const Matrix &Q,
+    const Matrix &R,
+    const Matrix &P)
     : A(A), C(C), Q(Q), R(R), P0(P),
-      m(C.rows()), n(A.rows()), dt(dt), initialized(false),
-      I(n, n), x_hat(n), x_hat_new(n)
+      m(C.rows()), n(A.rows()), A_t(C.rows(), C.rows()), b_t(P.rows(), 1),
+      dt(dt), initialized(false),
+      x_hat(A.rows(), 1), x_hat_new(A.rows(), 1)
 {
-    I.setIdentity();
+    I = Matrix::eye(n);
 }
 
+/**
+ * @brief Create a blank Estimator.
+ * 
+ */
 KalmanFilter::KalmanFilter() {}
 
-void KalmanFilter::init(double t0, const Eigen::VectorXd &x0)
+// -------------------------------- //
+//         PUBLIC FUNCTIONS         //
+// -------------------------------- //
+
+/**
+ * @brief Initialize the filter with initial states as zero.
+ * 
+ */
+void KalmanFilter::init(double t0, const Matrix &x0)
 {
     x_hat = x0;
-    P = P0;
-    this->t0 = t0;
-    t = t0;
-    initialized = true;
+    setInitState(t0, P0);
 }
 
+/**
+ * @brief Initialize the filter with a guess for initial states.
+ * 
+ */
 void KalmanFilter::init()
 {
-    x_hat.setZero();
-    P = P0;
-    t0 = 0;
-    t = t0;
-    initialized = true;
+    setInitState(0, P0);
 }
 
-void KalmanFilter::update(const Eigen::VectorXd &y)
+/**
+ * @brief Update the estimated state based on measured values. The
+ * time step is assumed to remain constant.
+ * 
+ */
+void KalmanFilter::update(const Matrix &y)
 {
 
     if (!initialized)
@@ -52,18 +95,32 @@ void KalmanFilter::update(const Eigen::VectorXd &y)
 
     x_hat_new = A * x_hat;
     P = A * P * A.transpose() + Q;
-    K = P * C.transpose() * (C * P * C.transpose() + R).inverse();
-    x_hat_new += K * (y - C * x_hat_new);
-    P = (I - K * C) * P;
+    A_t = C * P * C.transpose();
+    b_t = P * C;
+
+    K = m_solver->solve(A_t, b_t);
+    x_hat_new += K * (y - C.transpose() * x_hat_new);
+    P = (I - K * C.transpose()) * P;
     x_hat = x_hat_new;
 
     t += dt;
 }
 
-void KalmanFilter::update(const Eigen::VectorXd &y, double dt, const Eigen::MatrixXd A)
+/**
+ * @brief Update the estimated state based on measured values,
+ * using the given time step and dynamics matrix.
+ * 
+ */
+void KalmanFilter::update(const Matrix &y, double dt, const Matrix A)
 {
-
     this->A = A;
     this->dt = dt;
     update(y);
 }
+
+/**
+ * @brief Return the current state and time.
+ * 
+ */
+Matrix KalmanFilter::state() { return x_hat; };
+double KalmanFilter::time() { return t; };
